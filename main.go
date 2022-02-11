c01ab3be681a664ee8b6b38d296e22914c6f715c
João Carvalho <5263301+jpldcarvalho@users.noreply.github.com>
2022-02-11 23:55:54 +0000

    add cors

--- main.go
+++ main.go
@@ -10,12 +10,19 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func CORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	godotenv.Load()
 	services.MongoClient.Init()
 	defer services.MongoClient.Close()
 
-	http.Handle("/", rest.Router())
+	http.Handle("/", CORS(rest.Router()))
 
 	log.Println("[+] Auth API running on port 8080")
 	http.ListenAndServe(":8080", nil)
